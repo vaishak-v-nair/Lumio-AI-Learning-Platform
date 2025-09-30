2fe95f814790e96f8c6e1cec67283d675421f4d4
VAISHAK V NAIR <vaishakpkd2004@gmail.com>
2025-09-30 06:06:06 +0000

    Few pages are not scrollable. Fix those.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       </head>
       <body
         className={cn(
-          'min-h-screen bg-background font-body antialiased overflow-hidden',
+          'min-h-screen bg-background font-body antialiased',
           inter.variable
         )}
       >
