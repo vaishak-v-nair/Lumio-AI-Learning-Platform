05a216af3d644a668f7a470bbb6f59d579d68231
VAISHAK V NAIR <vaishakpkd2004@gmail.com>
2025-10-04 14:49:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5138391784-a103a",
+  "appId": "1:790007585173:web:b51e5e8e9b286b9c273e9c",
+  "apiKey": "AIzaSyBXOWfqWWQpCVdvKhKEBM1tEVdpAHYryjc",
+  "authDomain": "studio-5138391784-a103a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "790007585173"
+};
