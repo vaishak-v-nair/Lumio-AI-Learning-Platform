43289a42c5efba5e8dd65fe83b2002b33c121a3b
VAISHAK V NAIR <vaishakpkd2004@gmail.com>
2025-10-04 13:28:45 +0000

    Add this to API key: AIzaSyDclZR0XipWYSTPIRX5DJLIN2i5aEwfOqE

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   "projectId": "studio-5138391784-a103a",
   "appId": "1:790007585173:web:b51e5e8e9b286b9c273e9c",
-  "apiKey": "AIzaSyBXOWfqWWQpCVdvKhKEBM1tEVdpAHYryjc",
+  "apiKey": "AIzaSyDclZR0XipWYSTPIRX5DJLIN2i5aEwfOqE",
   "authDomain": "studio-5138391784-a103a.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "790007585173"
